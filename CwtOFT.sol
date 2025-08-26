42cf0ecbea6e75d5b9acf21407c313583d77d11c
Serg-Maximchuk <serg.maximchuk@gmail.com>
2025-08-26 10:28:41 +0300

    fixed `Ownable` issue

--- CwtOFT.sol
+++ CwtOFT.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-import { OFT } from "https://raw.githubusercontent.com/LayerZero-Labs/LayerZero-v2/200cda254120375f40ed0a7e89931afb897b8891/packages/layerzero-v2/evm/oapp/contracts/oft/OFT.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
 
-contract CwtOFT is OFT {
-    /// @param lzEndpoint LayerZero EndpointV2 (BSC)
-    /// @param owner_     owner (your BSC Safe)
-    constructor(address lzEndpoint, address owner_)
-    OFT("ColdWallet Token", "CWT", lzEndpoint, owner_)
-    {}
+/// @notice OFT is an ERC-20 token that extends the OFTCore contract.
+contract MyOFT is OFT {
+        /// @param _lzEndpoint LayerZero EndpointV2 (BSC)
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _lzEndpoint,
+        address _owner
+    ) OFT(_name, _symbol, _lzEndpoint, _owner) Ownable(_owner) {}
 }
--- CwtOFTAdapter.sol
+++ CwtOFTAdapter.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-// LZ v2, pinned commit
-import { OFTAdapter } from "https://raw.githubusercontent.com/LayerZero-Labs/LayerZero-v2/200cda254120375f40ed0a7e89931afb897b8891/packages/layerzero-v2/evm/oapp/contracts/oft/OFTAdapter.sol";
+import { OFTAdapter } from "@layerzerolabs/oft-evm/contracts/OFTAdapter.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title CWT OFT Adapter (Ethereum) — locks canonical ERC-20 and bridges via LZ v2
 contract CwtOFTAdapter is OFTAdapter {
-    /// @param token      canonical CWT (ERC-20) on Ethereum
-    /// @param lzEndpoint LayerZero EndpointV2 (Ethereum)
-    /// @param owner_     contract owner (your Ethereum Safe)
-    constructor(address token, address lzEndpoint, address owner_)
-    OFTAdapter(token, lzEndpoint, owner_)
-    {}
+    /// @param _token      canonical CWT (ERC-20) on Ethereum
+    /// @param _lzEndpoint LayerZero EndpointV2 (Ethereum)
+    /// @param _owner     contract owner
+    constructor(
+        address _token,
+        address _lzEndpoint,
+        address _owner
+    ) OFTAdapter(_token, _lzEndpoint, _owner) Ownable(_owner) {}
 }
